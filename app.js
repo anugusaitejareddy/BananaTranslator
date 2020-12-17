8fb29419cb01273180bdf55ce2ca74f79f8bfab2
saiteja <saitejareddynov30@gmail.com>
2020-12-17 23:03:11 +0530

    error handler

--- app.js
+++ app.js
@@ -8,13 +8,26 @@ function getTranslationURL(text)
     return url + "?" + "text=" + text;
 }
 
+function errorHandler(error)
+{
+    console.log(error);
+    alert("only five request are accepted per hour, please wait");
+}
+
 async function clickHandler()
 {
     const userInput = document.getElementById("user-input").value;
-    const response = await fetch(getTranslationURL(userInput));
-    const object = await response.json();
-    console.log(object);
-    translatedTextarea.value = object.contents.translated;
+    
+    // const response = await fetch(getTranslationURL(userInput));
+    // const object = await response.json();
+    // response.json().catch(errorHandler);
+
+    fetch(getTranslationURL(userInput))
+    .then(response => response.json)
+    .then(json => {
+        translatedTextarea.value = object.contents.translated;
+    }).catch(errorHandler);
+
 }
 
 translateBtn.addEventListener("click", clickHandler);
